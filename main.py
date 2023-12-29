6381e0b5ac6ab4a38ef578d356270fe01d7784b6
imsk <65082478+imskGithub@users.noreply.github.com>
2023-12-29 22:46:49 +0530

    Added History. Now it only downloads the image if it doesn't matched the image stored in history.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import requests
 import os
 from bs4 import BeautifulSoup as bs
@@ -34,8 +35,42 @@ def Instagram_Save(username, date=date):
 def Instagram_Saves(filename="list.txt", date=date, sep="\n"):
     with open(f"{filename}", "r") as file:
         dump = file.read()
-    for i in dump.split(sep):
-        Instagram_Save(i, date)
+
+    history = {}
+    if not os.path.exists("history.json"):
+        for i in dump.split(sep):
+            Instagram_Save(i, date)
+            history[str(i)] = str(date)
+    else:
+        with open("history.json", "r") as file:
+            history = json.load(file)
+
+        for i in dump.split(sep):
+            if not (str(i) in history):
+                Instagram_Save(i, date)
+                history[str(i)] = str(date)
+            else:
+                history_date = str(history[str(i)])
+                if not os.path.exists(history_date):
+                    Instagram_Save(i, date)
+                    history[str(i)] = str(date)
+                else:
+                    if not os.path.exists(f"{history_date}/{str(i)}.jpg"):
+                        Instagram_Save(i, date)
+                        history[str(i)] = str(date)
+                    else:
+                        with open(f"{history_date}/{str(i)}.jpg", "rb") as file:
+                            history_image = file.read()
+                        link = getImageLink(i)
+                        r = requests.get(link)
+                        if history_image == r.content:
+                            pass
+                        else:
+                            writeImage(link, i, date)
+                            history[str(i)] = date
+
+    with open("history.json", "w") as file:
+        json.dump(history, file)
 
 
 if not os.path.exists(date):
@@ -43,3 +78,8 @@ if not os.path.exists(date):
 
 # Example - Instagram_Save("youtube")
 # Example - Instagram_Saves(filename="list.txt", sep="\n")
+
+
+# Write your code above this!
+if not os.listdir(date):
+    os.rmdir(date)
